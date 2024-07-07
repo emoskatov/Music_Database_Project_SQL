043a3d2e49c847179cf1a70433ff5ef0a2cb77f7
emoskatov <94897447+emoskatov@users.noreply.github.com>
2024-07-07 19:00:29 +0300

    add Select and Insert SQL query

---
+++ Insert.sql
@@ -0,0 +1,116 @@
+INSERT INTO
+    music_genre (name)
+VALUES
+    ('Jazz'),
+    ('Electronic dance music'),
+    ('Pop'),
+    ('Rock'),
+    ('Reggae'),
+    ('Country'),
+    ('Punk'),
+    ('Blues'),
+    ('Hip hop'),
+    ('Folk'),
+    ('Soul');
+
+INSERT INTO
+    artist (name)
+VALUES
+    ('Louis Armstrong'),
+    ('Avicii'),
+    ('Michael Jackson'),
+    ('The Beatles'),
+    ('Bob Marley'),
+    ('Johnny Cash'),
+    ('Ramones'),
+    ('John Lee Hooker'),
+    ('Eminem'),
+    ('Bob Dylan'),
+    ('Aretha Franklin'),
+    ('Miles Davis'),
+    ('Duke Ellington'),
+    ('Drake'),
+    ('Kanye West'); -- без связи с жанром 
+
+INSERT INTO
+    artist_genre (artist_id, music_genre_id) -- генерация связей исполнителя и жанра c помощью генератора
+VALUES
+    (generate_series(1, 11), generate_series(1, 11));
+
+INSERT INTO
+    artist_genre (artist_id, music_genre_id)
+VALUES
+    (12, 1),
+    (13, 1),
+    (14, 9),
+    (3,4),
+    (3,11),
+    (3,2);
+
+INSERT INTO
+    album (title, year_of_release)
+VALUES
+    ('Ella and Louis', 1956),
+    ('Stories', 2015),
+    ('Thriller', 1982),
+    ('Abbey Road', 1969),
+    ('Natty Dread', 1974),
+    ('Songs of Our Soil', 1959),
+    ('Mondo Bizarro', 1992),
+    ('Jealous', 1987),
+    ('Music to Be Murdered By', 2020),
+    ('Modern Times', 2006),
+    ('Aretha', 1986);
+
+INSERT INTO
+    artist_album (artist_id, album_id) -- генерация связей исполнителя и альбома для основного задания
+VALUES
+    (generate_series(1, 11), generate_series(1, 11));
+
+INSERT INTO
+    track (title, duration, album_id)
+VALUES
+    ('Isn’t This a Lovely Day?', '6:14', 1),
+    ('Talk to Myself', '3:55', 2),
+    ('Thriller', '5:57', 3),
+    ('Come Together', '4:19', 4),
+    ('Lively Up Yourself', '5:29', 5),
+    ('Drink to Me', '1:54', 6),
+    ('Anxiety', '2:04', 7),
+    ('Ninety Days', '3:24', 8),
+    ('In Too Deep', '3:15', 9),
+    ('Spirit on the Wate', '7:42', 10),
+    ('If You Need My Love Tonigh', '4:30', 11),
+    ('Do You Still Remember','5:10',11),
+    ('Nettie Moore','6:53',10),
+    ('Marsh','3:21',9),
+    ('Early One Morning','4:02',8);
+
+INSERT INTO
+    collection (name, year_of_release)
+VALUES
+    ('popular track 2020 year', 2020),
+    ('popular track 2019 year', 2019),
+    ('popular track 2010 year', 2010),
+    ('popular track 2005 year', 2005);
+
+INSERT INTO
+    track_collection (track_id, collection_id)
+VALUES
+    (1, 1),
+    (2, 1),
+    (3, 1),
+    (4, 1),
+    (2, 2),
+    (3, 2),
+    (4, 2),
+    (5, 2),
+    (5, 3),
+    (6, 3),
+    (7, 3),
+    (8, 4),
+    (9, 4);
+
+-- Дополнительные команды которые могут понадобится 
+-- Если мы удаляем данные из таблицы, инкремент запоминает последний испольщованный ID поэтому при тестировании возникла необходимость сбросить счетчик принудительно
+--ALTER SEQUENCE collection_id_seq RESTART WITH 1; Сбрасывается этой командой где таблица_столбец_seq, необходимую таблицу так же можно посмотреть в DBveaver в таблице по умолчанию в данном поле
---
+++ Select.sql
@@ -0,0 +1,168 @@
+-- Задание 2.1
+SELECT
+    title,
+    to_char(duration, 'MI:SS') AS duration -- с приведением к формату минуты:секунды
+FROM
+    track
+WHERE
+    duration = (
+        SELECT
+            max(duration)
+        FROM
+            track
+    );
+
+-- Задание 2.2   
+SELECT
+    title,
+    to_char(duration, 'MI:SS') AS duration -- с приведением к формату минуты:секунды
+FROM
+    track
+WHERE
+    -- также приведение к нужному нам интервалу, можно использовать как явное указание минут и секунд или приведение как указано в столбце при создании ::INTERVAL MINUTE TO SECOND
+    duration >= '3m:30s';
+
+-- Задание 2.3   
+SELECT
+    name
+FROM
+    collection
+WHERE
+    year_of_release BETWEEN 2018
+    AND 2020;
+
+-- Задание 2.4   
+SELECT
+    name
+FROM
+    artist
+WHERE
+    name NOT LIKE '% %';
+
+-- Задание 2.5   
+SELECT
+    title,
+    album_id
+FROM
+    track
+WHERE
+    -- Сделал приведение к нижнему регистру для нерегистрозависимому поиску
+    lower(title) LIKE '% my %'
+    OR lower(title) LIKE '% мой %';
+
+-- Задание 3.1
+SELECT
+    mg.name,
+    count(a.name)
+FROM
+    music_genre AS mg
+    JOIN artist_genre AS ag ON mg.id = ag.music_genre_id -- Можно опустить INNER так как он стоит по умолчанию и подходит нам, не выбираем то что не сооттносится
+    JOIN artist AS a ON ag.artist_id = a.id
+GROUP BY
+    mg.name
+ORDER BY
+    mg.name;
+
+-- Задание 3.2   
+SELECT
+    count(t.title)
+FROM
+    album AS a
+    JOIN track AS t ON a.id = t.album_id
+WHERE
+    a.year_of_release BETWEEN 2019
+    AND 2020;
+
+-- Задание 3.3   
+SELECT
+    a.title,
+    to_char(avg(t.duration), 'MI:SS') AS duration
+FROM
+    album AS a
+    JOIN track AS t ON a.id = t.album_id -- Расчет происходит корректно, проверено дополнительно, за счет приведения была отброшена часть с микросекундами, без округления
+GROUP BY
+    a.title;
+
+-- Задание 3.4   
+SELECT
+    a.name
+FROM
+    artist AS a
+    JOIN artist_album AS aa ON a.id = aa.artist_id
+    JOIN album AS a2 ON a2.id = aa.album_id -- Так должно быть быстрее для запроса когда мы в Join пишем условие, так как отсекается часть данных на этапе соединения таблиц
+    AND NOT a2.year_of_release = 2020;
+
+-- Задание 3.5   
+SELECT
+    c.name AS name_collection
+FROM
+    artist_album AS aa
+    JOIN artist AS a ON aa.artist_id = a.id
+    AND a.name = 'Bob Marley' -- Исполнителем выбран Bob Marley
+    JOIN album AS a2 ON a2.id = aa.album_id
+    JOIN track AS t ON t.album_id = a2.id
+    JOIN track_collection AS tc ON tc.track_id = t.id
+    JOIN collection AS c ON c.id = tc.collection_id;
+
+-- Дополнительное задание 4.1
+SELECT
+    a2.title
+FROM
+    artist AS a
+    JOIN artist_genre AS ag ON ag.artist_id = a.id
+    JOIN artist_album AS aa ON a.id = aa.artist_id
+    JOIN album AS a2 ON a2.id = aa.album_id
+GROUP BY
+    a2.title
+HAVING
+    count(ag.artist_id) > 1;
+
+--Дополнительное задание 4.2
+SELECT
+    t.title
+FROM
+    track AS t
+    LEFT JOIN track_collection AS tc ON t.id = tc.track_id
+WHERE
+    tc.track_id IS NULL;
+
+--Дополнительное задание 4.3 
+SELECT
+    a2.name,
+    t.duration
+FROM
+    album AS a
+    JOIN track AS t ON t.album_id = a.id
+    AND t.duration = (
+        SELECT
+            min(duration)
+        FROM
+            track
+    )
+    JOIN artist_album AS aa ON aa.album_id = a.id
+    JOIN artist AS a2 ON aa.artist_id = a2.id;
+
+-- Дополнительное задание 4.4
+SELECT
+    t.album_id
+FROM
+    track AS t
+    JOIN album AS a ON a.id = t.album_id
+GROUP BY
+    t.album_id
+HAVING
+    count(t.album_id) = (
+        SELECT
+            count(t.album_id) AS count
+        FROM
+            track AS t
+            JOIN album AS a ON a.id = t.album_id
+        GROUP BY
+            t.album_id
+        ORDER BY
+            count
+        LIMIT
+            1
+    )
+ORDER BY
+    t.album_id;
